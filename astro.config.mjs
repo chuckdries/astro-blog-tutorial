972719c22c469ff4418d078b1c4d92d74ea75496
Chuck Dries <chuck@chuckdries.com>
2023-07-03 17:26:41 -0700

    dark mode and preact component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
+import preact from "@astrojs/preact"
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://chuckdries.github.io',
-  base: '/astro-blog-tutorial'
+  base: '/astro-blog-tutorial',
+  integrations: [preact()]                    
 });
